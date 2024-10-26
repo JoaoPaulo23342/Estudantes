50696ac608f7995d9f68bd6211cc73b4c95df948
João <Joaopauloresendecosta55@gmail.com>
2024-10-26 13:23:26 -0300

    adicionando eventos no botao

--- organo/src/components/TextArea/TextArea.js
+++ organo/src/components/TextArea/TextArea.js
@@ -1,10 +1,14 @@
 import './TextArea.css'
 
 const TextArea = (props) => {
+    
+    const aoDigitado =  (ev) => {
+        console.log(ev.target.value)
+    }
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} type="text" placeholder={props.placeholder}/>
+            <input onChange={aoDigitado} required={props.obrigatorio} type="text" placeholder={props.placeholder}/>
         </div>
     )
 }
