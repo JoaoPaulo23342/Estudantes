7bf041475ce83422f5df74ad4018b79dc5408fde
João <Joaopauloresendecosta55@gmail.com>
2024-10-31 15:20:07 -0300

    corrigindo erros Finalmente

--- organo/src/components/Estudantes/Index.js
+++ organo/src/components/Estudantes/Index.js
@@ -6,7 +6,7 @@ const Estudantes = (props) => {
         
         <section className='estudante' style={{ backgroundColor: props.secondaryColor }}>
             <h3 style={{ borderColor: props.primaryColor }}>{props.nome}</h3>
-            {props.Estudantes.map(Estudante => <Card />)}
+            {props.estudantes.map(Estudante => <Card />)}
         </section>
     )
 }
