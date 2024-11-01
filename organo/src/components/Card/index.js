b437e9288ad464a7321bae63a0be469cfd5c3054
João <Joaopauloresendecosta55@gmail.com>
2024-11-01 16:53:57 -0300

    adicionando props

--- organo/src/components/Card/index.js
+++ organo/src/components/Card/index.js
@@ -1,14 +1,14 @@
 import './Card.css'
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='estudante1'>
             <div className='cabecalho'>
                 <img src='https://github.com/JoaoPaulo23342.png' alt=''></img>
             </div>
             <div className='rodape'>
-                <h4>jp</h4>
-                <h5>Ciencia da computação</h5>
+                <h4>{props.nome}</h4>
+                <h5>{props.formacaoAcademica}</h5>
             </div>
         </div>
     )
