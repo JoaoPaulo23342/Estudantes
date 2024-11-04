9acfa4f7b70dd94d3031c008b36a7e726815fb01
João <Joaopauloresendecosta55@gmail.com>
2024-11-04 12:27:04 -0300

    ocultando as faculdades vazias

--- organo/src/components/Estudantes/Index.js
+++ organo/src/components/Estudantes/Index.js
@@ -4,7 +4,7 @@ import './estilo.css'
 const Estudantes = (props) => {
     return (
         
-        <section className='estudante' style={{ backgroundColor: props.secondaryColor }}>
+        props.estudantes.length > 0 && <section className='estudante' style={{ backgroundColor: props.secondaryColor }}>
             <h3 style={{ borderColor: props.primaryColor }}>{props.nome}</h3>
             <div className='estudantesCard'>
             {props.estudantes.map(estudante => <Card key={estudante.nome} nome={estudante.nome} formacaoAcademica={estudante.formacaoAcademica} linkedin={estudante.linkedin} />)}
