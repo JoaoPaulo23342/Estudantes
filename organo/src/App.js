1787c9885b0e58134ef7654feda7f063134c06cb
João <Joaopauloresendecosta55@gmail.com>
2024-11-05 13:23:23 -0300

    deploy

--- organo/src/App.js
+++ organo/src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Formulario from './Formulario';
 
-import Estudantes from './components/Estudantes';
+import Estudantes from './components/index.js';
 import rodape from './components/Footer';
 import Footer from './components/Footer';
 
